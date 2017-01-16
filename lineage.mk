813459a647a5bc5d57dab63b6b9593d29d9b5729
lirokoa <lirokoa@hotmail.com>
2017-01-16 12:19:56 +0100

    rebrand for lineage OS

--- lineage.mk
+++ lineage.mk
@@ -9,10 +9,10 @@ $(call inherit-product, device/samsung/n8013/full_n8013.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n8013
-PRODUCT_NAME := cm_n8013
+PRODUCT_NAME := lineage_n8013
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N8013
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-N8013 TARGET_DEVICE=GT-N8013 BUILD_FINGERPRINT="samsung/p4noterfxx/p4noterf:4.0.4/IMM76D/N8000XWALG9:user/release-keys" PRIVATE_BUILD_DESC="samsung/p4notewifiue/p4notewifi:4.0.4/IMM76D/N8013UEALH2:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-N8013 TARGET_DEVICE=GT-N8013 BUILD_FINGERPRINT="samsung/p4noterfxx/p4noterf:4.4.2/KOT49H/N8000XXUDNE4:user/release-keys" PRIVATE_BUILD_DESC="p4noterfxx-user 4.4.2 KOT49H N8000XXUDNE4 release-keys"
